237cc7ff8e2e279fbe92e216d88fb241a8efd66b
dev-may <zermth@gmail.com>
2025-04-30 20:30:40 +0700

    remove table

--- app/carpool-management/page.tsx
+++ app/carpool-management/page.tsx
@@ -1,14 +1,12 @@
 "use client";
-import LoginHeader from "@/components/loginHeader";
-import BackButton from "@/components/backButton";
-import RequestListTable from "@/components/tables/request-list-table";
+
 
 export default function CarpoolManagement() {
 
   return (
     <div className="page-container">
+test
 
-        <RequestListTable />
     </div>
   );
 }
