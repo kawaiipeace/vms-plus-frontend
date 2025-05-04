fcccaea3f81980f034714abd8ab4bfdd4ff25731
dev-may <zermth@gmail.com>
2025-05-04 15:40:02 +0700

    fix logs and edit

--- components/tabs/requestEditTab.tsx
+++ components/tabs/requestEditTab.tsx
@@ -1,28 +1,16 @@
 import React, { useEffect, useState } from "react";
 import RequestDetailForm from "@/components/flow/requestDetailForm";
-import { LogType } from "@/app/types/log-type";
 import LogListTable from "@/components/table/log-list-table";
-import { PaginationType } from "@/app/types/request-action-type";
-import { fetchLogs } from "@/services/masterService";
 import PaginationControls from "../table/pagination-control";
+import { useLogContext } from "@/contexts/log-context";
 
 interface Props {
   requestId: string;
 }
 
 export default function RequestEditTabs({ requestId }: Props) {
-  const [params, setParams] = useState({
-    page: 1,
-    limit: 10,
-  });
-  const [requestUid] = useState(requestId);
-  const [dataRequest, setDataRequest] = useState<LogType[]>([]);
-  const [pagination, setPagination] = useState<PaginationType>({
-    limit: 10,
-    page: 1,
-    total: 0,
-    totalPages: 0,
-  });
+    const { dataRequest, pagination, params, setParams, loadLogs } =
+      useLogContext();
 
   const handlePageChange = (newPage: number) => {
     setParams((prevParams) => ({
@@ -43,28 +31,10 @@ export default function RequestEditTabs({ requestId }: Props) {
 
 
   useEffect(() => {
-    const fetchRequests = async () => {
-      try {
-        const response = await fetchLogs(requestUid, params);
-        const requestList = response.data.logs;
-        const { total, totalPages } = response.data;
-  
-        setDataRequest(requestList);
-        setPagination({
-          limit: params.limit,
-          page: params.page,
-          total,
-          totalPages,
-        });
-      } catch (error) {
-        console.error("Error fetching requests:", error);
-      }
-    };
-  
     if (requestId) {
-      fetchRequests();
+      loadLogs(requestId);
     }
-  }, [params, requestUid]);
+  }, [params, requestId]);
 
   const tabs = [
     {
