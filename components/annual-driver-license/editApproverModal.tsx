141f6a5560fcb58230331291b89bf3963e20c3a0
dev-may <zermth@gmail.com>
2025-05-19 01:19:11 +0700

    update

--- components/annual-driver-license/editApproverModal.tsx
+++ components/annual-driver-license/editApproverModal.tsx
@@ -210,7 +210,7 @@ const EditApproverModal = forwardRef<
                         readOnly
                         {...field}
                       />
-                    )}s
+                    )}
                   />
                 </div>
               </div>
