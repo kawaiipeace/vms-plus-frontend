3bd8f77612ecdd7410dbb424dc719e7978ab8c02
Chonlada Dangdee <chonlada.da.work@gmail.com>
2025-05-24 13:14:12 +0700

    fix: 🐛 error login with thaiID

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import axios from 'axios';
+import axios from "axios";
 
 // Function to get the API configuration
 export const getApiConfig = () => {
   const fallback = {
-    baseURL: 'http://pntdev.ddns.net:28084/api',
-    apiKey: '2c5SF8BDhWKzdTY5MIFXEh9PummQMhK8w2TIUobJnYbAxaUmYo1sYTc2Hwo3xNWj',
-    callbackURL: 'http://localhost/callback_code_token',
+    baseURL: "http://pntdev.ddns.net:28084/api",
+    apiKey: "2c5SF8BDhWKzdTY5MIFXEh9PummQMhK8w2TIUobJnYbAxaUmYo1sYTc2Hwo3xNWj",
+    callbackURL: "http://localhost/callback_code_token",
   };
 
-  if (typeof window !== 'undefined' && window.__ENV__) {
+  if (typeof window !== "undefined" && window.__ENV__) {
     return {
       baseURL: window.__ENV__.NEXT_PUBLIC_CLIENT_API_HOST || fallback.baseURL,
       apiKey: window.__ENV__.NEXT_PUBLIC_API_KEY || fallback.apiKey,
@@ -20,11 +20,10 @@ export const getApiConfig = () => {
   return fallback;
 };
 
-
 const axiosInstance = axios.create({
   headers: {
-    accept: 'application/json',
-    'Content-Type': 'application/json',
+    accept: "application/json",
+    "Content-Type": "application/json",
   },
 });
 
@@ -33,15 +32,15 @@ axiosInstance.interceptors.request.use(
   (config) => {
     const { baseURL, apiKey } = getApiConfig();
     config.baseURL = baseURL;
-    config.headers['X-ApiKey'] = apiKey;  
+    config.headers["X-ApiKey"] = apiKey;
 
-    const token = localStorage.getItem('accessToken');
+    const token = localStorage.getItem("accessToken");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => Promise.reject(error),
+  (error) => Promise.reject(error)
 );
 
 // Flag to prevent multiple refresh calls at the same time
@@ -67,6 +66,10 @@ axiosInstance.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
+    if (error.response?.status === 401 && !originalRequest._retry && originalRequest.url === "login/authen-thaiid") {
+      return Promise.reject(error);
+    }
+
     // If error is 401 (Unauthorized), try refreshing the token
     if (error.response?.status === 401 && !originalRequest._retry) {
       if (isRefreshing) {
@@ -85,9 +88,9 @@ axiosInstance.interceptors.response.use(
       isRefreshing = true;
 
       try {
-        const refreshToken = localStorage.getItem('refreshToken');
+        const refreshToken = localStorage.getItem("refreshToken");
         if (!refreshToken) {
-          throw new Error('No refresh token available');
+          throw new Error("No refresh token available");
         }
 
         // Send refresh token request
@@ -96,7 +99,7 @@ axiosInstance.interceptors.response.use(
         });
 
         const newAccessToken = refreshResponse.data.accessToken;
-        localStorage.setItem('accessToken', newAccessToken);
+        localStorage.setItem("accessToken", newAccessToken);
 
         // Process the queue with the new token
         processQueue(null, newAccessToken);
@@ -106,9 +109,9 @@ axiosInstance.interceptors.response.use(
         return axiosInstance(originalRequest);
       } catch (err) {
         processQueue(err);
-        localStorage.removeItem('accessToken');
-        localStorage.removeItem('refreshToken');
-        window.location.href = '/';
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        window.location.href = "/";
         return Promise.reject(err);
       } finally {
         isRefreshing = false;
@@ -116,7 +119,7 @@ axiosInstance.interceptors.response.use(
     }
 
     return Promise.reject(error);
-  },
+  }
 );
 
 export default axiosInstance;
