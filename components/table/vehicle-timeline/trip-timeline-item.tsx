78780acf74f18a73611a0d2c62e02e79ec7c0b4d
Ratchadech Sittikongkrittakorn <ratchadech@freakout.net>
2025-06-13 00:52:38 +0700

    fix trip-timeline-item

--- components/table/vehicle-timeline/trip-timeline-item.tsx
+++ components/table/vehicle-timeline/trip-timeline-item.tsx
@@ -19,19 +19,19 @@ const TripTimelineItem = ({
 }) => {
   const statusColors = STATUS_DETAIL_MAP[item.status];
 
-  const daysBetweenInclusive = (a: dayjs.ConfigType, b: dayjs.ConfigType) => dayjs(b).diff(dayjs(a), 'day') + 1;
+  const daysBetweenInclusive = (a: dayjs.ConfigType, b: dayjs.ConfigType) => dayjs(b).diff(dayjs(a), "day") + 1;
   const actualDuration = Math.min(
     daysBetweenInclusive(startDate, endDate),
-    daysBetweenInclusive(startDate, dayjs(timelineEndDate).toDate()),
+    daysBetweenInclusive(startDate, dayjs(timelineEndDate).toDate())
   );
 
   return (
     <button
       onClick={onClick}
-      className={`bg-[${statusColors.bg}] border border-[${statusColors.border}] top-0 left-0 !rounded-lg justify-start !cursor-pointer`}
+      className={`bg-[${statusColors?.bg}] border border-[${statusColors?.border}] top-0 left-0 !rounded-lg justify-start !cursor-pointer`}
       style={{ width: actualDuration * 150 }}
     >
-      <div className={`flex items-center gap-1 text-sm font-semibold text-[${statusColors.text}] py-[2px] px-[4px]`}>
+      <div className={`flex items-center gap-1 text-sm font-semibold text-[${statusColors?.text}] py-[2px] px-[4px]`}>
         <div className="flex flex-col">
           <i className="material-symbols-outlined !text-base !leading-4">directions_car</i>
           <i className="material-symbols-outlined !text-base !leading-4">person</i>
@@ -44,6 +44,5 @@ const TripTimelineItem = ({
     </button>
   );
 };
-
 
 export default TripTimelineItem;
