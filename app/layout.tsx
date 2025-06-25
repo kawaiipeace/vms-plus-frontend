0f67da58cdf647b2524f2c9f2b207ce8ced56115
dev-may <zermth@gmail.com>
2025-06-26 05:55:38 +0800

    change api

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="light-mode"> {/* Add className here too */}
+    <html lang="en" className="light-mode">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <Script id="env-config" src="/env/env-config.js" strategy="beforeInteractive" />
