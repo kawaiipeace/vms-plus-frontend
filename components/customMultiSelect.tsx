fed5e88b57d2c2eba52d737ad4828da7ebf5a7b2
natchanon.pan <natchanon.pabu@gmail.com>
2025-07-08 23:07:10 +0700

    ...

--- components/customMultiSelect.tsx
+++ components/customMultiSelect.tsx
@@ -136,7 +136,7 @@ export default function CustomMultiSelect({
         </div>
 
         <div className="flex-shrink-0 w-8 text-right">
-          <i className="material-symbols-outlined">keyboard_arrow_down</i>
+          {/* <i className="material-symbols-outlined">keyboard_arrow_down</i> */}
         </div>
       </div>
 
