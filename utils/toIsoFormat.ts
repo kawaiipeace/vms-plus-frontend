ec651b0cac04eaf7a1120395383b33c64d73fc64
dev-may <zermth@gmail.com>
2025-07-23 21:50:09 +0700

    update time

--- utils/toIsoFormat.ts
+++ utils/toIsoFormat.ts
@@ -1,6 +1,5 @@
 export function toISODateTime(date: string, time: string): string {
-    // Combine date and time, assume local time
     const localDate = new Date(`${date}T${time}:00`);
-    return localDate.toISOString(); // Converts to UTC format with .000Z
+    return localDate.toISOString().replace('.000', '');
   }
   
